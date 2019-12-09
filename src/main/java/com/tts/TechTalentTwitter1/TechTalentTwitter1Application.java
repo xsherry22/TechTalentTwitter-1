6d072f219cfbd94d55c2fdb52a165ead09ea7fe7
Sherry Xiong <Sherry@Sherrys-iMac.local>
2019-12-09 08:15:49 -0500

    first commit

---
+++ src/main/java/com/tts/TechTalentTwitter1/TechTalentTwitter1Application.java
@@ -0,0 +1,13 @@
+package com.tts.TechTalentTwitter1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTalentTwitter1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechTalentTwitter1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/TechTalentTwitter1/TechTalentTwitter1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.TechTalentTwitter1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechTalentTwitter1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
